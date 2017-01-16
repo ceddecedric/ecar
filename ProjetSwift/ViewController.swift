dbdd1539993e9ddbf886789c09ac913b597173ed
Mac OS X <macosx@Macs-Mac.local>
2017-01-16 09:38:35 +0100

    Initial Commit

---
+++ ProjetSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjetSwift
+//
+//  Created by Mac OS X on 16/01/2017.
+//  Copyright © 2017 Mac OS X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
